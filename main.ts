2ce3d3661486b6dac3953b886ab69e5b3ece1cce
Badr Bouslikhin <bouslikhin.badr@gmail.com>
2021-01-29 00:23:09 +0100

    feat: add setting tab with number of files to show

--- main.ts
+++ main.ts
@@ -1,13 +1,18 @@
-import { MarkdownView, Plugin } from "obsidian";
+import { App, MarkdownView, Plugin, PluginSettingTab, Setting } from "obsidian";
 import dayjs from "dayjs";
 
 export default class Changelog extends Plugin {
+  setting: ChangelogSettings;
   onInit() {}
 
   onload() {
     console.log("loading plugin");
 
+    this.setting = {
+      numberOfFilesToShow: 10,
     };
+
+    this.addSettingTab(new ChangelogSettingsTab(this.app, this));
 
     this.addCommand({
       id: "write-changelog-to-current-note",
@@ -53,5 +58,36 @@ export default class Changelog extends Plugin {
 
   onunload() {
     console.log("unloading plugin");
+  }
+}
+
+interface ChangelogSettings {
+  numberOfFilesToShow: number;
+}
+
+class ChangelogSettingsTab extends PluginSettingTab {
+  plugin: Changelog;
+
+  constructor(app: App, plugin: Changelog) {
+    super(app, plugin);
+    this.plugin = plugin;
+  }
+
+  display(): void {
+    const { containerEl } = this;
+    const settings = this.plugin.setting;
+    new Setting(containerEl)
+      .setName("Number of recent files")
+      .setDesc("Number of most recently edited files to show in the changelog")
+      .addText((text) =>
+        text
+          .setValue(String(settings.numberOfFilesToShow))
+          .onChange((value) => {
+            if (!isNaN(Number(value))) {
+              settings.numberOfFilesToShow = Number(value);
+              this.plugin.saveData(settings);
+            }
+          })
+      );
   }
 }
