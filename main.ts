cfe65348dcb0a54e2deae4ff2f39e7845279d41f
Badr Bouslikhin <bouslikhin.badr@gmail.com>
2021-01-29 00:22:17 +0100

    refactor: move file load code to function

--- main.ts
+++ main.ts
@@ -1,37 +1,36 @@
 import { MarkdownView, Plugin } from "obsidian";
 import dayjs from "dayjs";
 
-export default class MyPlugin extends Plugin {
+export default class Changelog extends Plugin {
+  onInit() {}
+
   onload() {
     console.log("loading plugin");
 
-    const writeChangelog = () => {
-      const files = this.app.vault.getMarkdownFiles();
-      const sorted_files = files
-        .sort((a, b) => (a.stat.mtime < b.stat.mtime ? 1 : -1))
-        .slice(0, 5);
-      this.writeChangelog(sorted_files);
     };
 
     this.addCommand({
       id: "write-changelog-to-current-note",
       name: "Write changelog to current note",
-      callback: writeChangelog,
+      callback: () => this.writeChangelog(),
       hotkeys: [],
     });
 
-    this.registerEvent(this.app.vault.on("modify", this.watchVault))
+    this.registerEvent(this.app.vault.on("modify", this.watchVault));
   }
 
   watchVault() {
     console.log("Vault has changed");
   }
 
-  writeChangelog(recentlyEditedFiles: any) {
+  writeChangelog() {
     console.log("Writing changelog");
+    const files = this.app.vault.getMarkdownFiles();
+    const recentlyEditedFiles = files
+      .sort((a, b) => (a.stat.mtime < b.stat.mtime ? 1 : -1))
+      .slice(0, this.setting.numberOfFilesToShow);
     const view = this.app.workspace.getActiveViewOfType(MarkdownView);
     if (view) {
-      // Do work here
       const editor = view.sourceMode.cmEditor;
 
       editor.execCommand("selectAll");
